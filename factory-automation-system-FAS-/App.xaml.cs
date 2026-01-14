c7a8b79f858b3e5c3eb6e7302bebb8aa622e8e96
JUNYEONG <JUNYEONG@DESKTOP-RBC8ITQ>
2026-01-14 10:08:09 +0900

    초기 커밋.

---
+++ factory-automation-system-FAS-/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace factory_automation_system_FAS_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ factory-automation-system-FAS-/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace factory_automation_system_FAS_.Views
+{
+    // 클래스 이름 앞에 partial이 반드시 있어야 합니다.
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // 이제 빨간 줄이 사라질 것입니다.
+            InitializeComponent();
+        }
+    }
+}
