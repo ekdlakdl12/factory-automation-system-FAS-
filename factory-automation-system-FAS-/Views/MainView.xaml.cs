1462a5f521bd63501b4d1fbfc47eb200f3924a98
JUNYEONG <JUNYEONG@DESKTOP-RBC8ITQ>
2026-01-27 12:20:59 +0900

    임시ui 완성 1번화면제외

---
+++ factory-automation-system-FAS-/Views/MainView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace factory_automation_system_FAS_.Views
+{
+    // 1. partial 키워드가 있는지 확인
+    // 2. 클래스 이름이 MainView 인지 확인
+    // 3. 부모 클래스가 UserControl 인지 확인
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+        private void ShowMain_Click(object sender, RoutedEventArgs e)
+        {
+            MainMonitorSection.Visibility = Visibility.Visible;
+            LogSection.Visibility = Visibility.Collapsed;
+            SettingSection.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowLog_Click(object sender, RoutedEventArgs e)
+        {
+            MainMonitorSection.Visibility = Visibility.Collapsed;
+            LogSection.Visibility = Visibility.Visible;
+            SettingSection.Visibility = Visibility.Collapsed;
+        }
+
+        private void ShowSetting_Click(object sender, RoutedEventArgs e)
+        {
+            MainMonitorSection.Visibility = Visibility.Collapsed;
+            LogSection.Visibility = Visibility.Collapsed;
+            SettingSection.Visibility = Visibility.Visible;
+        }
+    }
+}
