edc0a136ebbb431988589452afd5970fbeb5977e
potoblue <potoblue@gmail.com>
2026-02-09 13:26:04 +0900

    바코드 컨버터

---
+++ SerialRead.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO.Ports;
+
+class SerialRead {
+    static void Main() {
+        SerialPort serialPort = new SerialPort("/dev/ttyAMA0", 9600);
+        SerialPort barcodeSerial = new SerialPort("/dev/ttyACM0", 9600);
+        barcodeSerial.DtrEnable = true;
+        barcodeSerial.RtsEnable = true;
+
+try {
+    barcodeSerial.Open();
+    serialPort.Open();
+
+    Console.WriteLine("Commnication...");
+
+    while (true) {
+        if (barcodeSerial.BytesToRead > 0) {
+            string barcodedata = barcodeSerial.ReadExisting(); 
+            
+            Console.WriteLine($"[Barcode In]: {barcodedata}");
+        
+            serialPort.Write(barcodedata);
+        }
+        if (serialPort.BytesToRead > 0) {
+            string serialdata = serialPort.ReadExisting();
+            Console.WriteLine($"[Serial Out]: {serialdata}");
+        }
+    }
+}
+catch (Exception ex) {
+    Console.WriteLine("error: " + ex.Message);
+}
+finally {
+    if (barcodeSerial.IsOpen) barcodeSerial.Close();
+    if (serialPort.IsOpen) serialPort.Close(); 
+}
+    }
+}
